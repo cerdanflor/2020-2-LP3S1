c30c848820eebfe776888d8769000bf9a44bd96e
Flor Cerdan León <cerdanflor@gmail.com>
2021-01-13 09:22:17 -0500

    Capa de Modelo - Generación de tablas Artículo y Categoria

--- ProyectosDJango/s07proyecto002/miapp/models.py
+++ ProyectosDJango/s07proyecto002/miapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Articulo(models.Model):
+    titulo = models.CharField(max_length=100)
+    contenido = models.TextField()
+    publicado = models.BooleanField()
+    # auto_now_add=True me pertmite 
+    # registrar la fecha de creación del registro
+    creado = models.DateTimeField(auto_now_add=True)
+    # auto_now=True me permite registrar
+    # la fecha cuando se modifique el registro
+    actualizado = models.DateTimeField(auto_now=True)
+
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.CharField(max_length=250)
+    # models.DateField() para guardar la fecha manualmente
+    creado = models.DateField()
