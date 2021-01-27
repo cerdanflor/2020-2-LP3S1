74d2d53c0b19a1054f0fd7659da8de4482a0cc77
Flor Cerdan León <cerdanflor@gmail.com>
2021-01-27 12:47:06 -0500

    Semana 14 Formulario Parte V - Validaciones de formularios 01

--- ProyectosDJango/s07proyecto002/miapp/forms.py
+++ ProyectosDJango/s07proyecto002/miapp/forms.py
@@ -2,13 +2,26 @@ from django import forms
 
 class FormArticulo(forms.Form):
     titulo = forms.CharField(
-        label="Titulo"
+        label="Titulo",
+        max_length=40,
+        required=False,
+        widget=forms.TextInput(
+            attrs={
+                'placeholder': 'Ingrese el título',
+                'class': 'titulo_form_articulo'
+            }
+        )
     )
 
     contenido = forms.CharField(
         label="Contenido",
-        widget=forms.Textarea
+        widget=forms.Textarea        
     )
+    contenido.widget.attrs.update({
+        'placeholder': 'Ingrese el contenido del artículo',
+        'class': 'contenido_form_articulo',
+        'id':'contenido_form'
+    })
 
     opciones_publicado = [
         (1,'Si'),
