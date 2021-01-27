8caf45b7b8db69f15fbcbf0112bb2ac5f33f7260
Flor Cerdan León <cerdanflor@gmail.com>
2021-01-27 08:57:03 -0500

    Semana 14 Formulario Parte I, Agregando la opción Crear Artícu

--- ProyectosDJango/Copia/miapp/templatetags/filters.py
+++
@@ -1,7 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.filter(name='saludo')
-def saludo(valor):
-    return f"<h1 style='background:green; color:white;'> Bienvenido, {valor} </h1>"
