9ea8633c08d686f45fe8cf6c593562e30604d83d
Flor Cerdan León <cerdanflor@gmail.com>
2021-01-27 12:59:46 -0500

    Semana 14 Formulario Parte V - Validaciones de formularios 02

--- ProyectosDJango/s07proyecto002/miapp/forms.py
+++ ProyectosDJango/s07proyecto002/miapp/forms.py
@@ -1,21 +1,29 @@
 from django import forms
+from django.core import validators
 
 class FormArticulo(forms.Form):
     titulo = forms.CharField(
         label="Titulo",
         max_length=40,
-        required=False,
+        required=True,
         widget=forms.TextInput(
             attrs={
                 'placeholder': 'Ingrese el título',
                 'class': 'titulo_form_articulo'
             }
-        )
+        ),
+        validators=[
+            validators.MinLengthValidator(4,'El título es corto'),
+            validators.RegexValidator('^[A-Za-z0-9ñÑ ]*$','El título tiene caracteres inválidos','titulo_invalido')
+        ]
     )
 
     contenido = forms.CharField(
         label="Contenido",
-        widget=forms.Textarea        
+        widget=forms.Textarea,
+        validators=[
+            validators.MaxLengthValidator(500,'Superaste el límite de caracteres')
+        ]        
     )
     contenido.widget.attrs.update({
         'placeholder': 'Ingrese el contenido del artículo',
